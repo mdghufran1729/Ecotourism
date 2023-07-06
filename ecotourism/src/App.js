e0e437b881a5bbd1ad227ed5622c36ea2c1a3936
mdghufran1729 <mdghufran1729@gmail.com>
2023-07-06 11:06:57 +0530

    basic

--- ecotourism/src/App.js
+++ ecotourism/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Card from "./components/CardComp"
+import Destination from './components/Destination';
 
 function App() {
+  var img="https://images.unsplash.com/photo-1519123721878-35e5fcfc2dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=80"
   return (
     <div className="App">
-      
+      <Destination/>
     </div>
   );
 }
