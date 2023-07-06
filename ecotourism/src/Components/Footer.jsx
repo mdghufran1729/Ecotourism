5e56c86ad3aca575427bc504102a0e3c367b6309
Imrajsrivastava <rajsri4485@gmail.com>
2023-07-06 16:09:47 +0530

    footerdone

--- ecotourism/src/Components/Footer.jsx
+++ ecotourism/src/Components/Footer.jsx
@@ -1,24 +1,24 @@
 import {
-    Box,
-    chakra,
-    Container,
-    Link,
-    SimpleGrid,
-    Stack,
-    Text,
-    VisuallyHidden,
-    Input,
-    IconButton,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import { ReactNode, useState } from 'react';
-  import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-  import { BiMailSend } from 'react-icons/bi';
-  import { SiGoogleearth } from 'react-icons/si';
-  import { AiTwotoneMail } from 'react-icons/ai';
-  import { useToast } from '@chakra-ui/react'
+  Box,
+  chakra,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactNode, useState } from "react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { BiMailSend } from "react-icons/bi";
+import { SiGoogleearth } from "react-icons/si";
+import { AiTwotoneMail } from "react-icons/ai";
+import { useToast } from "@chakra-ui/react";
+import { Link as Rlink } from "react-router-dom";
 
-  
 //   const Logo = (props: any) => {
 //     return (
 //       <svg
@@ -37,134 +37,161 @@ import {
 //       </svg>
 //     );
 //   };
-  
-  const SocialButton = ({
-    children,
-    label,
-    href,
-  }: {
-    children: ReactNode;
-    label: string;
-    href: string;
-  }) => {
-    return (
-      <chakra.button
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        w={8}
-        h={8}
-        cursor={'pointer'}
-        as={'a'}
-        href={href}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        }}>
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    );
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode,
+  label: string,
+  href: string,
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function Footer() {
+  const [input, setInput] = useState("");
+
+  const Stoast = useToast({
+    title: `Your information has been safely stored!`,
+    status: "success",
+    isClosable: true,
+    position: "top",
+  });
+
+  const hamdleEmail = () => {
+    setInput("");
+    Stoast();
   };
-  
-  const ListHeader = ({ children }: { children: ReactNode }) => {
-    return (
-      <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
-        {children}
-      </Text>
-    );
-  };
-  
-  export default function Footer() {
-    const [input,setInput] = useState('')
-
-    const Stoast = useToast(  {
-        title: `Your information has been safely stored!`,
-        status: "success",
-        isClosable: true,
-        position:'top'
-      })
-
-    const hamdleEmail = ()=>{
-
-        setInput('');
-        Stoast()
-
-    }
-    return (
-      <Box
-        bg={useColorModeValue('gray.50', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}>
-        <Container as={Stack} maxW={'6xl'} py={10}>
-          <SimpleGrid
-            templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
-            spacing={8}>
-            <Stack spacing={6}>
-              <Box>
-                {/* <Logo color={useColorModeValue('gray.700', 'white')} /> */}
-               {/* <SiGoogleearth/>Ecotourism */}
-              </Box>
-              <SiGoogleearth/>
-              <Text fontSize={'sm'}>
-                © 2023  Ecotourism. All rights reserved
-              </Text>
-              <Stack direction={'row'} spacing={6}>
-                <SocialButton label={'Twitter'} href={'#'}>
-                  <FaTwitter />
-                </SocialButton>
-                <SocialButton label={'YouTube'} href={'#'}>
-                  <FaYoutube />
-                </SocialButton>
-                <SocialButton label={'Instagram'} href={'#'}>
-                  <FaInstagram />
-                </SocialButton>
-              </Stack>
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+          spacing={8}
+        >
+          <Stack spacing={6}>
+            <Text
+              as={Rlink}
+              to={"/"}
+              fontFamily={"heading"}
+              color={useColorModeValue("teal", "white")}
+              // mr={'120px'}
+              ml={"25px"}
+              fontSize={"xl"}
+              fontWeight={800}
+            >
+              <SiGoogleearth
+                size={30}
+                style={{ marginLeft: "65px", marginBottom: "-26px" }}
+              />{" "}
+              Ecotourism
+            </Text>
+            <Text style={{textAlign:"left",fontSize:"16px"}}>
+              Welcome to our travel website, your ultimate destination for all
+              your travel needs! Whether you're a seasoned explorer or a
+              first-time traveler, we are here to inspire and assist you on your
+              journey. Our platform offers a wide range of services, including
+              flight bookings, hotel accommodations, vacation packages, car
+              rentals, and much more. With our user-friendly interface and
+              comprehensive search options, finding the perfect travel options
+              has never been easier. So, get ready to embark on unforg
+            </Text>
+            <Text fontSize={"sm"}>© 2023 Ecotourism. All rights reserved</Text>
+            <Stack direction={"row"} spacing={6}>
+              <SocialButton label={"Twitter"} href={"#"}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={"YouTube"} href={"#"}>
+                <FaYoutube />
+              </SocialButton>
+              <SocialButton label={"Instagram"} href={"#"}>
+                <FaInstagram />
+              </SocialButton>
             </Stack>
-            <Stack align={'flex-start'}>
-              <ListHeader>Company</ListHeader>
-              <Link href={'#'}>About us</Link>
-              <Link href={'#'}>Blog</Link>
-              <Link href={'#'}>Contact us</Link>
-              <Link href={'#'}>Pricing</Link>
-              <Link href={'#'}>Testimonials</Link>
-            </Stack>
-            <Stack align={'flex-start'}>
-              <ListHeader>Support</ListHeader>
-              <Link href={'#'}>Help Center</Link>
-              <Link href={'#'}>Terms of Service</Link>
-              <Link href={'#'}>Legal</Link>
-              <Link href={'#'}>Privacy Policy</Link>
-              <Link href={'#'}>Satus</Link>
-            </Stack>
-            <Stack align={'flex-start'}>
-              <ListHeader>Contact Us<AiTwotoneMail/></ListHeader>
-              <Stack direction={'row'}>
-                <Input
-                onChange={(e)=>{setInput(e.target.value)}}
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Link href={"#"}>About us</Link>
+            <Link href={"#"}>Blog</Link>
+            <Link href={"#"}>Contact us</Link>
+            <Link href={"#"}>Pricing</Link>
+            <Link href={"#"}>Testimonials</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Support</ListHeader>
+            <Link href={"#"}>Help Center</Link>
+            <Link href={"#"}>Terms of Service</Link>
+            <Link href={"#"}>Legal</Link>
+            <Link href={"#"}>Privacy Policy</Link>
+            <Link href={"#"}>Satus</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>
+              Contact Us
+              <AiTwotoneMail />
+            </ListHeader>
+            <Stack direction={"row"}>
+              <Input
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
                 value={input}
-                  placeholder={'Your email address'}
-                  bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-                  border={0}
-                  _focus={{
-                    bg: 'whiteAlpha.300',
-                  }}
-                />
-                <IconButton
-                onClick={()=>{hamdleEmail()}}
-                  bg={useColorModeValue('green.400', 'green.800')}
-                  color={useColorModeValue('white', 'gray.800')}
-                  _hover={{
-                    bg: 'green.600',
-                  }}
-                  aria-label="Subscribe"
-                  icon={<BiMailSend />}
-                />
-              </Stack>
+                placeholder={"Your email address"}
+                bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+                border={0}
+                _focus={{
+                  bg: "whiteAlpha.300",
+                }}
+              />
+              <IconButton
+                onClick={() => {
+                  hamdleEmail();
+                }}
+                bg={useColorModeValue("green.400", "green.800")}
+                color={useColorModeValue("white", "gray.800")}
+                _hover={{
+                  bg: "green.600",
+                }}
+                aria-label="Subscribe"
+                icon={<BiMailSend />}
+              />
             </Stack>
-          </SimpleGrid>
-        </Container>
-      </Box>
-    );
-  }
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
