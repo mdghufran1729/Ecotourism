1185ee6a9aeb668cba7231756df3a5b73e5c3990
Imrajsrivastava <rajsri4485@gmail.com>
2023-07-07 21:17:03 +0530

    done

--- ecotourism/src/Components/Navbar.jsx
+++ ecotourism/src/Components/Navbar.jsx
@@ -14,15 +14,22 @@ import {
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 import {
   HamburgerIcon,
   CloseIcon,
   ChevronDownIcon,
   ChevronRightIcon,
-} from "@chakra-ui/icons";
+ 
+} from '@chakra-ui/icons';
 
-import { Link as Rlink } from "react-router-dom";
+import { SiGoogleearth } from 'react-icons/si';
+
+
+
+
+
+import {Link as Rlink} from 'react-router-dom'
 
 export default function WithSubnavigation() {
   const { isOpen, onToggle } = useDisclosure();
@@ -30,89 +37,89 @@ export default function WithSubnavigation() {
   return (
     <Box>
       <Flex
-        bg={useColorModeValue("white", "gray.800")}
-        color={useColorModeValue("gray.600", "white")}
-        minH={"80px"}
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'80px'}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
-        backgroundColor={"#7DBBAB"}
-        //  style={{backgroundImage: "linear-gradient(to right , #215f37, #125037, #0c4134, #0f332d, #132423)"}}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        backgroundColor={'#7dbbab'}
+      //  style={{backgroundImage: "linear-gradient(to right , #215f37, #125037, #0c4134, #0f332d, #132423)"}}
 
-        align={"center"}
-      >
+        align={'center'}>
         <Flex
-          flex={{ base: 1, md: "auto" }}
+          flex={{ base: 1, md: 'auto' }}
           ml={{ base: -2 }}
-          display={{ base: "flex", md: "none" }}
-        >
+          display={{ base: 'flex', md: 'none' }}>
           <IconButton
             onClick={onToggle}
             icon={
-              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              isOpen ? <CloseIcon w={3} h={3}  /> : <HamburgerIcon w={5} h={5}  />
             }
-            variant={"ghost"}
-            aria-label={"Toggle Navigation"}
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
             mr={"70px"}
           />
         </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+       
+         
           <Text
-            as={Rlink}
-            to={"/"}
-            textAlign={useBreakpointValue({ base: "center", md: "left" })}
-            fontFamily={"heading"}
-            color={useColorModeValue("white", "white")}
-            mr={"120px"}
+             as={Rlink}
+             to={'/'}
+            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+            fontFamily={'heading'}
+            color={useColorModeValue('white', 'white')}
+            mr={'120px'}
             ml={"55px"}
-            fontSize={"lg"}
+            fontSize={'lg'}
             fontWeight={800}
-          >
-            Ecotourism
+            >
+        {/* <SiGoogleearth style={{marginTop:"10px"}}/>  */}
+        <SiGoogleearth style={{marginLeft:"-30px",marginBottom:"-26px"}} size={30}/> Ecotourism
           </Text>
+         
 
-          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
           </Flex>
         </Flex>
 
         <Stack
           flex={{ base: 1, md: 0 }}
-          justify={"flex-end"}
-          direction={"row"}
-          spacing={6}
-        >
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}>
           <Button
             as={Rlink}
-            to={"/login"}
-            fontSize={"lg"}
+            to={'/login'}
+            fontSize={'lg'}
             fontWeight={500}
-            color={"whiteAlpha.700"}
-            variant={"link"}
+            color={"whiteAlpha.900"}
+            variant={'link'}
             // href={'/login'}
-          >
+            >
             Log In
           </Button>
           <Button
-            as={Rlink}
-            to={"/register"}
-            display={{ base: "none", md: "inline-flex" }}
-            fontSize={"lg"}
+             as={Rlink}
+             to={'/register'}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'lg'}
             fontWeight={600}
-            color={"white"}
+            color={'white'}
             p={"5"}
             // bg={'orange.400'}
             mr={"55px"}
             // href={'/register'}
-            borderRadius={"20px"}
-            backgroundColor={"#face54"}
-            //   style={{backgroundImage: "linear-gradient(to right, #e26109, #e9701c, #ef7e2c, #f68c3a, #fc9949)"}}
+borderRadius={"20px"}
+backgroundColor={'#face54'}
+          //   style={{backgroundImage: "linear-gradient(to right, #e26109, #e9701c, #ef7e2c, #f68c3a, #fc9949)"}}
             _hover={{
-              bg: "orange.300",
-            }}
-          >
+              bg: 'orange.300',
+            }}>
             Book Now
           </Button>
         </Stack>
@@ -126,29 +133,26 @@ export default function WithSubnavigation() {
 }
 
 const DesktopNav = () => {
-  const linkColor = useColorModeValue("whiteAlpha.700", "white");
-  const linkHoverColor = useColorModeValue("gray.200", "white");
-  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+  const linkColor = useColorModeValue('whiteAlpha.900', 'white');
+  const linkHoverColor = useColorModeValue('gray.200', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
   return (
-    <Stack direction={"row"} spacing={4}>
+    <Stack direction={'row'} spacing={4}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger={"hover"} placement={"bottom-start"}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
             <PopoverTrigger>
-              <Link
-                as={Rlink}
-                to={navItem.href}
+              <Link as={Rlink} to={navItem.href}
                 p={10}
                 // href={navItem.href ?? '#'}
-                fontSize={"lg"}
+                fontSize={'lg'}
                 fontWeight={500}
                 color={linkColor}
                 _hover={{
-                  textDecoration: "none",
+                  textDecoration: 'none',
                   color: linkHoverColor,
-                }}
-              >
+                }}>
                 {navItem.label}
               </Link>
             </PopoverTrigger>
@@ -156,12 +160,11 @@ const DesktopNav = () => {
             {navItem.children && (
               <PopoverContent
                 border={0}
-                boxShadow={"xl"}
+                boxShadow={'xl'}
                 bg={popoverContentBgColor}
                 p={4}
-                rounded={"xl"}
-                minW={"sm"}
-              >
+                rounded={'xl'}
+                minW={'sm'}>
                 <Stack>
                   {navItem.children.map((child) => (
                     <DesktopSubNav key={child.label} {...child} />
@@ -180,33 +183,30 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   return (
     <Link
       href={href}
-      role={"group"}
-      display={"block"}
+      role={'group'}
+      display={'block'}
       p={2}
-      rounded={"md"}
-      _hover={{ bg: useColorModeValue("orange.50", "gray.900") }}
-    >
-      <Stack direction={"row"} align={"center"}>
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('teal.50', 'gray.200') }}>
+      <Stack direction={'row'} align={'center'}>
         <Box>
           <Text
-            transition={"all .3s ease"}
-            _groupHover={{ color: "orange.400" }}
-            fontWeight={500}
-          >
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'teal.300' }}
+            fontWeight={500}>
             {label}
           </Text>
-          <Text fontSize={"sm"}>{subLabel}</Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
         </Box>
         <Flex
-          transition={"all .3s ease"}
-          transform={"translateX(-10px)"}
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
           opacity={0}
-          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
-          justify={"flex-end"}
-          align={"center"}
-          flex={1}
-        >
-          <Icon color={"orange.400"} w={5} h={5} as={ChevronRightIcon} />
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}>
+          <Icon color={'orange.400'} w={5} h={5} as={ChevronRightIcon} />
         </Flex>
       </Stack>
     </Link>
@@ -216,10 +216,9 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
 const MobileNav = () => {
   return (
     <Stack
-      bg={useColorModeValue("white", "gray.800")}
+      bg={useColorModeValue('white', 'gray.500')}
       p={4}
-      display={{ md: "none" }}
-    >
+      display={{ md: 'none' }}>
       {NAV_ITEMS.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
@@ -235,39 +234,36 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
       <Flex
         py={2}
         as={Link}
-        href={href ?? "#"}
-        justify={"space-between"}
-        align={"center"}
+        href={href ?? '#'}
+        justify={'space-between'}
+        align={'center'}
         _hover={{
-          textDecoration: "none",
-        }}
-      >
+          textDecoration: 'none',
+        }}>
         <Text
           fontWeight={600}
-          color={useColorModeValue("gray.600", "gray.200")}
-        >
+          color={useColorModeValue('gray.600', 'gray.200')}>
           {label}
         </Text>
         {children && (
           <Icon
             as={ChevronDownIcon}
-            transition={"all .25s ease-in-out"}
-            transform={isOpen ? "rotate(180deg)" : ""}
+            transition={'all .25s ease-in-out'}
+            transform={isOpen ? 'rotate(180deg)' : ''}
             w={6}
             h={6}
           />
         )}
       </Flex>
 
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
         <Stack
           mt={2}
           pl={4}
           borderLeft={1}
-          borderStyle={"solid"}
-          borderColor={useColorModeValue("gray.200", "gray.700")}
-          align={"start"}
-        >
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          align={'start'}>
           {children &&
             children.map((child) => (
               <Link key={child.label} py={2} href={child.href}>
@@ -289,45 +285,45 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "Home",
+    label: 'Home',
     children: [
       {
-        label: "Explore Design Work",
-        subLabel: "Trending Design to inspire you",
-        href: "#",
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#',
       },
       {
-        label: "New & Noteworthy",
-        subLabel: "Up-and-coming Designers",
-        href: "#",
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#',
       },
     ],
   },
   {
-    label: "Destinations",
+    label: 'Destinations',
     children: [
       {
-        label: "Job Board",
-        subLabel: "Find your dream design job",
-        href: "#",
+        label: 'Job Board',
+        subLabel: 'Find your dream design job',
+        href: '#',
       },
       {
-        label: "Freelance Projects",
-        subLabel: "An exclusive list for contract work",
-        href: "#",
+        label: 'Freelance Projects',
+        subLabel: 'An exclusive list for contract work',
+        href: '#',
       },
     ],
   },
   {
-    label: "Activities",
-    href: "/learn",
+    label: 'Activities',
+    href: '/learn',
   },
   {
-    label: "Packages",
-    href: "/learn",
-  },
+      label: 'Packages',
+      href: '/learn',
+    },
   {
-    label: "About Us",
-    href: "hire",
+    label: 'About Us',
+    href: 'hire',
   },
 ];
